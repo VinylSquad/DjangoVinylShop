ca327d34764ed246a83ddda7459dba86bd2e2932
Marek Jarczewski <marek.jarczewski81@gmail.com>
2023-08-03 04:02:50 +0200

    Add category view in store/views.py & base.html

--- DjangoVinylShop_project/store/views.py
+++ DjangoVinylShop_project/store/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
+from . models import Category
+
 # Create your views here.
 
 def store(request):
     
-    return render(request, 'store/store.html' )
+    return render(request, 'store/store.html')
+
+
+
+def categories(request):
+    
+    all_categories = Category.objects.all()
+    
+    return {'all_categories': all_categories}
+
+# After that go to context processor in setting.py/TEMPLATES and add your views
