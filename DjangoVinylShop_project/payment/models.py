5cfd7aff4751961276b98da0b629ce5855c60f4c
tomas185 <tomas185@o2.pl>
2023-08-05 14:20:07 +0200

    Added models Order and OrderItem

--- DjangoVinylShop_project/payment/models.py
+++ DjangoVinylShop_project/payment/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+from store.models import Product
+
 
 class ShippingAddress(models.Model):
     full_name = models.CharField(max_length=300)
     email = models.EmailField(max_length=255)
     street = models.CharField(max_length=300)
-    street_number = models.CharField(max_length=10)
+    street_number = models.DecimalField(max_digits=8, decimal_places=0)
     city = models.CharField(max_length=255)
     zipcode = models.CharField(max_length=255, null=True, blank=True) 
 
@@ -24,5 +26,29 @@ class ShippingAddress(models.Model):
         return 'Shipping Address - ' + str(self.id)
 
     
-    # shipping address (1)
-    # shipping address (2)
+class Order(models.Model):
+    full_name = models.CharField(max_length=300)
+    email = models.EmailField(max_length=255)
+    shipping_adress = models.TextField(max_length=10000)
+    amount_paid = models.DecimalField(max_digits=8, decimal_places=2)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+    # foreign key
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return 'Order - #' + str(self.id)
+
+class OrderItem(models.Model):
+    # foreign key ->
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+
+    quantity = models.PositiveBigIntegerField(default=1)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    # foreign key
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return 'Order Item - #' + str(self.id)
--- DjangoVinylShop_project/payment/views.py
+++ DjangoVinylShop_project/payment/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
+from . models import ShippingAddress
 
 # Create your views here.
 
 def checkout(request):
-    return render(request, 'payment/checkout.html')
+    # users with account -> pre-fill form
+    if request.user.is_authenticated:
+        try:
+            # authenticated users with shipping information
+            shipping_address = ShippingAddress.objects.get(user=request.user.id)
+            context = {'shipping': shipping_address}
+            return render(request, 'payment/checkout.html', context=context)
+        
+        except:
+            # authinticated user with no shipping information
+            return render(request, 'payment/checkout.html')
+        
+    else:
+        # guest user -> not logged
+        return render(request, 'payment/checkout.html')
 
 def payment_success(request):
     return render(request, 'payment/payment-success.html')
