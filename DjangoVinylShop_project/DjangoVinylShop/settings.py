b11f9cfa113c8bfe7618d5c7402b96ff45eb4999
LukaWrob <133563131+LukaWrob@users.noreply.github.com>
2023-08-05 18:11:49 +0200

    Email verification final

--- DjangoVinylShop_project/DjangoVinylShop/settings.py
+++ DjangoVinylShop_project/DjangoVinylShop/settings.py
@@ -140,4 +140,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Email verification settings
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = 'True'
+
+EMAIL_HOST_USER = 'wrobleluka@gmail.com'
+EMAIL_HOST_PASSWORD = 'sfqeybeenbtoizxi'
